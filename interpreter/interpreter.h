27a1cb3fdd144c45d9cef044ce19c3de7a6eac45
Christopher Crawford <chriscraws@gmail.com>
2020-05-21 11:03:03 -0700

    add stub c++ interpreter library

---
+++ interpreter/interpreter.h
@@ -0,0 +1,15 @@
+// Copyright 2020 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SSIR_INTERPRETER_INTERPRETER_H_
+#define SSIR_INTERPRETER_INTERPRETER_H_
+
+namespace ssir {
+
+// Stub interpreter class.
+class Interpreter {};
+
+}  // namespace ssir
+
+#endif // SSIR_INTERPRETER_INTERPRETER_H_

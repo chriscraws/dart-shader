c7b4abefd45811ac9dbad19afffac486b885bde8
Christopher Crawford <chriscraws@gmail.com>
2020-05-29 10:47:21 -0700

    a few const constructors

--- expr/lib/src/expression.dart
+++ expr/lib/src/expression.dart
@@ -48,9 +48,38 @@ abstract class Expression<T extends Type> {
   void _writeTo(Shader shader);
 }
 
+class _Const<T extends Type> extends Expression<T> {
+  _Const(this._value);
+
+  final List<int> _value;
+
+  void _writeTo(Shader shader) {
+    shader._context[this] = shader._buffer.length;
+    shader._buffer
+      ..add(_vectorType)
+      ..addAll(_value);
+  }
+}
+
+/// Namespace for Const expression constructors.
+class Const {
+  /// Constant scalar expression.
+  static Expression<Scalar> scalar(double value) => _Const<Scalar>([value]);
+
+  /// Constant vec2 expression.
+  static Expression<Vec2> vec2(double x, double y) => _Const<Vec2>([x, y]);
+
+  /// Constant vec3 expression.
+  static Expression<Vec3> vec2(double x, double y, double z) =>
+      _Const<Vec3>([x, y, z]);
+
+  /// Constant vec4 expression.
+  static Expression<Vec2> vec2(double x, double y, double z, double w) =>
+      _Const<Vec4>([x, y, z, w]);
+}
+
 // A specification for a shader that can be serialized to SSIR.
 class Shader {
-
   Shader({this.fragmentColor});
 
   /// The output color for a fragment shader.

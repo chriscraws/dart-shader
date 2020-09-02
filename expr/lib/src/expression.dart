8d9e1ceec279b3e54eb83603fa3290452cbc5ff7
Christopher Crawford <chriscraws@gmail.com>
2020-09-02 15:25:05 -0400

    simplify class struture

--- expr/lib/src/expression.dart
+++ expr/lib/src/expression.dart
@@ -1,74 +1,107 @@
-import 'dart:typed_data';
 import 'package:expr/src/spirv.dart' as spirv;
-import 'package:expr/src/constants.dart';
-
-/// Specifies the numerical type of an instance of [Expression].
-abstract class Type {
-  VectorType get _vectorType;
-  int get _vectorDimensions => vectorTypeDimensions[_vectorType];
-}
-
-/// Floating point number.
-class Scalar extends Type {
-  VectorType get _vectorType => VectorType.scalar;
-}
-
-/// Two-component vector.
-class Vec2 extends Type {
-  VectorType get _vectorType => VectorType.vec2;
-}
-
-/// Three-component vector.
-class Vec3 extends Type {
-  VectorType get _vectorType => VectorType.vec3;
-}
-
-/// Four-component vector.
-class Vec4 extends Type {
-  VectorType get _vectorType => VectorType.vec4;
-}
-
-/// Two-by-two matrix.
-class Mat2 extends Type {
-  VectorType get _vectorType => VectorType.mat2;
-}
-
-/// Three-by-three matrix.
-class Mat3 extends Type {
-  VectorType get _vectorType => VectorType.mat3;
-}
-
-/// Four-by-four matrix.
-class Mat4 extends Type {
-  VectorType get _vectorType => VectorType.mat4;
-}
-
-// Specifies the precision of an expression.
-abstract class Precision {
-  PrecisionType get _precisionType;
-}
-
-// Low precision
-class LowP extends Precision {
-  PrecisionType get _precisionType => PrecisionType.low;
-}
-
-// Medium precision
-class MediumP extends Precision {
-  PrecisionType get _precisionType => PrecisionType.medium;
-}
-
-// High precision
-class HighP extends Precision {
-  PrecisionType get _precisionType => PrecisionType.high;
-}
 
 /// Node within an SSIR abstract syntax tree.
-abstract class Expression<P extends Precision, T extends Type> {
-  final T _type;
-  final P _precision;
+mixin _Expression {
+  spirv.Instruction get _instruction;
+  spirv.Type get _type;
+}
 
-  Expression._(this._precision, this._type);
+abstract class Scalar with _Expression {
+  Scalar._();
 
-  spirv.Instruction toInstruction();
+  spirv.Type get _type => spirv.floatT;
+
+  factory Scalar(double value) => _ConstScalar(value);
+
+  Scalar operator +(Scalar b) => _Scalar(_Add(this, b));
+  Scalar operator -(Scalar b) => _Scalar(_Subtract(this, b));
+  Scalar operator *(Scalar b) => _Scalar(_Multiply(this, b));
+  Scalar operator /(Scalar b) => _Scalar(_Divide(this, b));
+}
+
+class _ConstScalar extends Scalar {
+  final double value;
+
+  _ConstScalar(this.value)
+      : assert(value != null),
+        super._();
+
+  spirv.Instruction get _instruction => spirv.OpConstant(value);
+}
+
+class _Scalar extends Scalar {
+  final _Expression _child;
+
+  _Scalar(this._child)
+      : assert(_child != null),
+        assert(_child._type == spirv.floatT),
+        super._();
+
+  spirv.Instruction get _instruction => _child._instruction;
+}
+
+abstract class Vec4 with _Expression {
+  Vec4._();
+
+  spirv.Type get _type => spirv.vec4T;
+
+  factory Vec4(double x, double y, double z, double w) =>
+      _ConstVec4(x, y, z, w);
+}
+
+class _ConstVec4 extends Vec4 {
+  final double x;
+  final double y;
+  final double z;
+  final double w;
+
+  _ConstVec4(this.x, this.y, this.z, this.w)
+      : assert(x != null),
+        assert(y != null),
+        assert(z != null),
+        assert(w != null),
+        super._();
+
+  spirv.Instruction get _instruction =>
+      spirv.OpConstantComposite.vec4(x, y, z, w);
+}
+
+abstract class _BinOp with _Expression {
+  final _Expression a;
+  final _Expression b;
+
+  _BinOp(this.a, this.b)
+      : assert(a != null),
+        assert(b != null),
+        assert(a._type == b._type);
+
+  spirv.Type get _type => a._type;
+}
+
+class _Add extends _BinOp {
+  _Add(_Expression a, _Expression b) : super(a, b);
+
+  spirv.Instruction get _instruction =>
+      spirv.OpFAdd(a._instruction, b._instruction);
+}
+
+class _Subtract extends _BinOp {
+  _Subtract(_Expression a, _Expression b) : super(a, b);
+
+  spirv.Instruction get _instruction =>
+      spirv.OpFSub(a._instruction, b._instruction);
+}
+
+class _Multiply extends _BinOp {
+  _Multiply(_Expression a, _Expression b) : super(a, b);
+
+  spirv.Instruction get _instruction =>
+      spirv.OpFMul(a._instruction, b._instruction);
+}
+
+class _Divide extends _BinOp {
+  _Divide(_Expression a, _Expression b) : super(a, b);
+
+  spirv.Instruction get _instruction =>
+      spirv.OpFDiv(a._instruction, b._instruction);
 }
--- expr/lib/src/spirv.dart
+++ expr/lib/src/spirv.dart
@@ -4,6 +4,7 @@
 // used in this package, it can be extended as necessary.
 
 import 'dart:convert';
+import 'dart:typed_data';
 
 final _magicNumber = 0x07230203;
 
@@ -144,10 +145,29 @@ class OpTypeFloat extends Instruction with Type {
 
   const OpTypeFloat._(this.bitWidth)
       : super._(
+          result: true,
           opCode: 22,
         );
 
   List<int> operands(Identifier i) => [bitWidth];
+}
+
+class OpTypeVec extends Instruction with Type {
+  final Type componentType;
+  final int dimensions;
+
+  const OpTypeVec._(this.componentType, this.dimensions)
+      : assert(componentType != null),
+        assert(dimensions > 1),
+        super._(
+          result: true,
+          opCode: 23,
+        );
+
+  List<int> operands(Identifier i) => [
+        i.identify(componentType),
+        dimensions,
+      ];
 }
 
 class Precision {
@@ -155,11 +175,12 @@ class Precision {
   const Precision._(this.bitWidth);
 }
 
-final lowP = Precision._(8);
-final mediumP = Precision._(12);
-final highP = Precision._(16);
+final mediumP = Precision._(32);
 
-final floatT = OpTypeFloat._(highP.bitWidth);
+final floatT = const OpTypeFloat._(32);
+final vec2T = OpTypeVec._(floatT, 2);
+final vec3T = OpTypeVec._(floatT, 3);
+final vec4T = OpTypeVec._(floatT, 4);
 
 const _opFunctionEnd = 1 << 16 | 56;
 
@@ -224,4 +245,81 @@ class Block {
         ...label.encode(i),
         ...termination.encode(i),
       ];
+}
+
+class OpConstant extends Instruction {
+  final double value;
+
+  OpConstant(this.value)
+      : super._(
+          type: floatT,
+          result: true,
+          opCode: 43,
+        );
+
+  List<int> operands(Identifier i) =>
+      Float32List.fromList([value]).buffer.asInt32List();
+}
+
+class OpConstantComposite extends Instruction {
+  final List<OpConstant> constituants;
+
+  OpConstantComposite._(Type type, List<double> constituants)
+      : assert(constituants != null),
+        assert(constituants.length > 1),
+        assert(constituants.every((c) => c != null)),
+        constituants = constituants.map((v) => OpConstant(v)).toList(),
+        super._(
+          type: type,
+          result: true,
+          opCode: 44,
+        );
+
+  OpConstantComposite.vec4(double x, double y, double z, double w)
+      : this._(vec4T, [x, y, z, w]);
+
+  List<int> operands(Identifier i) =>
+      constituants.map((op) => i.identify(op)).toList();
+}
+
+// Numerical operation with one arguments.
+class UniOp extends Instruction {
+  final Instruction a;
+
+  UniOp(int opCode, this.a)
+      : super._(
+          type: a.type,
+          result: true,
+          opCode: opCode,
+        );
+}
+
+// Numerical operation with two arguments.
+class BinOp extends Instruction {
+  final Instruction a;
+  final Instruction b;
+
+  BinOp(int opCode, this.a, this.b)
+      : assert(a.type == b.type),
+        super._(
+          type: a.type,
+          result: true,
+          opCode: opCode,
+        );
+}
+
+class OpFAdd extends BinOp {
+  OpFAdd(Instruction a, Instruction b) : super(129, a, b);
+}
+
+class OpFSub extends BinOp {
+  OpFSub(Instruction a, Instruction b) : super(131, a, b);
+}
+
+class OpFMul extends BinOp {
+  OpFMul(Instruction a, Instruction b) : super(133, a, b);
+}
+
+class OpFDiv extends BinOp {
+  OpFDiv(Instruction a, Instruction b) : super(136, a, b);
 }

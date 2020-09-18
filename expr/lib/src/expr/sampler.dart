f9d67191cdb4a7671ea1328f29d9ba246022b03b
Christopher Crawford <chriscraws@gmail.com>
2020-09-18 16:59:28 -0400

    [expr] add test for sampler, get rid of FutureOr bug

--- expr/lib/src/expr/sampler.dart
+++ expr/lib/src/expr/sampler.dart
@@ -3,7 +3,8 @@ part of '../../expr.dart';
 /// Sample from an [Image]
 class Sampler {
   Image _image;
-  FutureOr<ByteData> _data;
+  Future<ByteData> _dataFuture;
+  ByteData _resolvedData;
   ShaderFunction _function;
 
   Sampler(Image image) {
@@ -13,27 +14,26 @@ class Sampler {
 
   Vec4 sample(Vec2 pos) => Vec4._(_function(pos._node));
 
-  Future<void> get ready async => _data;
+  Future<void> get ready async => _dataFuture;
 
   Future<void> setImage(Image image) async {
     assert(image != null);
     _image = image;
-    _data = image.toByteData();
-    await _data;
+    _dataFuture = image.toByteData();
+    _resolvedData = await _dataFuture;
   }
 
   void _evaluator(double x, double y, List<double> result) {
-    if (_data == null || _data is Future<ByteData>) {
+    if (_resolvedData == null) {
       return;
     }
-    final bytes = _data as ByteData;
     final xi = (x * _image.width).floor() % _image.width;
     final yi = (y * _image.height).floor() % _image.height;
     final byteIndex = (xi + yi * _image.width) * 4;
-    assert(byteIndex < bytes.lengthInBytes);
-    result[0] = bytes.getUint8(byteIndex + 0).toDouble() / 255.0;
-    result[1] = bytes.getUint8(byteIndex + 1).toDouble() / 255.0;
-    result[2] = bytes.getUint8(byteIndex + 2).toDouble() / 255.0;
-    result[3] = bytes.getUint8(byteIndex + 3).toDouble() / 255.0;
+    assert(byteIndex < _resolvedData.lengthInBytes);
+    result[0] = _resolvedData.getUint8(byteIndex + 0).toDouble() / 255.0;
+    result[1] = _resolvedData.getUint8(byteIndex + 1).toDouble() / 255.0;
+    result[2] = _resolvedData.getUint8(byteIndex + 2).toDouble() / 255.0;
+    result[3] = _resolvedData.getUint8(byteIndex + 3).toDouble() / 255.0;
   }
 }

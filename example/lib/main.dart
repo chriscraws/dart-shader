74822c50462b4c24a23b4cdb6b97cdcf46314c2a
Christopher Crawford <chriscraws@gmail.com>
2020-11-04 12:26:42 -0800

    [example] format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -46,48 +46,51 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
-	final shader = MainShader();
+  final shader = MainShader();
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-	final notifier = ChangeNotifier();
-	Ticker ticker;
-	AnimationController _controller;
+  final notifier = ChangeNotifier();
+  Ticker ticker;
+  AnimationController _controller;
 
-	void initState() {
-		super.initState();
-		_controller = AnimationController(
-			vsync: this,
-			duration: Duration(seconds: 2),
-		)..addStatusListener((status) {
-			if (status == AnimationStatus.completed) {
-        _controller.reverse();
-			} else if (status == AnimationStatus.dismissed) {
-				_controller.forward();
-			}
-		})..forward();
-		final curvedAnim = CurvedAnimation(
-			parent: _controller,
-			curve: Curves.bounceOut,
-			reverseCurve: Curves.bounceIn,
-		);
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    )
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+      })
+      ..forward();
+    final curvedAnim = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.bounceOut,
+      reverseCurve: Curves.bounceIn,
+    );
 
-		ticker = createTicker((duration) {
-			notifier.notifyListeners();
-			widget.shader.time.value = duration.inMilliseconds.toDouble() / 1000.0;
-			
-			final pos = widget.shader.circlePos;
-			pos.value.x = -(curvedAnim.value - 0.5) * 0.2;
-		})..start();
-	}
+    ticker = createTicker((duration) {
+      notifier.notifyListeners();
+      widget.shader.time.value = duration.inMilliseconds.toDouble() / 1000.0;
 
-	void dispose() {
-		_controller.dispose();
-		super.dispose();
-	}
+      final pos = widget.shader.circlePos;
+      pos.value.x = -(curvedAnim.value - 0.5) * 0.2;
+    })
+      ..start();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,77 +102,75 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-				child: CustomPaint(
-						painter: ShaderPainter(
-							ssirShader: widget.shader,
-							shader: FragmentShader.spirv(
-								widget.shader.toSPIRV().asUint8List()),
-							repaint: notifier,
-							resolution: widget.shader.resolution,
-						),
-						size: Size.infinite,
-					),
-				),
+        child: CustomPaint(
+          painter: ShaderPainter(
+            ssirShader: widget.shader,
+            shader: FragmentShader.spirv(widget.shader.toSPIRV().asUint8List()),
+            repaint: notifier,
+            resolution: widget.shader.resolution,
+          ),
+          size: Size.infinite,
+        ),
+      ),
     );
   }
 }
 
 class MainShader extends Shader {
-	final time = ScalarUniform();
-	final resolution = Vec2Uniform();
-	final circlePos = Vec2Uniform();
+  final time = ScalarUniform();
+  final resolution = Vec2Uniform();
+  final circlePos = Vec2Uniform();
 
-	Scalar dcircle(Vec2 p, Scalar r) => p.length() - r;
+  Scalar dcircle(Vec2 p, Scalar r) => p.length() - r;
 
-	Vec4 color(Vec2 position) {
-		final aspect = resolution.x / resolution.y;
-		Vec2 p = position / resolution.x.v2 -
-			Vec2.of(0.5.s, 0.5.s / aspect);
+  Vec4 color(Vec2 position) {
+    final aspect = resolution.x / resolution.y;
+    Vec2 p = position / resolution.x.v2 - Vec2.of(0.5.s, 0.5.s / aspect);
 
-		p -= circlePos;
-		p += Vec2.of(0.s, 0.03.s * sin(time + 30.s * p.y));
+    p -= circlePos;
+    p += Vec2.of(0.s, 0.03.s * sin(time + 30.s * p.y));
 
-		final pixel = 1.s / resolution.x;
-		final b = dcircle(p, 0.13.s).smoothStep(0.0.s, pixel);
-		return Vec4.of([
-			b.v3,
-			1.s,
-		]);
-	}
+    final pixel = 1.s / resolution.x;
+    final b = dcircle(p, 0.13.s).smoothStep(0.0.s, pixel);
+    return Vec4.of([
+      b.v3,
+      1.s,
+    ]);
+  }
 }
 
 extension E on num {
-	Scalar get s => Scalar(this.toDouble());
-	Vec2 get v2 => Vec2.all(this.toDouble());
+  Scalar get s => Scalar(this.toDouble());
+  Vec2 get v2 => Vec2.all(this.toDouble());
 }
 
 class ShaderPainter extends CustomPainter {
-	final Shader ssirShader;
-	final FragmentShader shader;
-	final Listenable repaint;
-	final Vec2Uniform resolution;
+  final Shader ssirShader;
+  final FragmentShader shader;
+  final Listenable repaint;
+  final Vec2Uniform resolution;
 
-	ShaderPainter({
-		this.ssirShader,
-		this.shader,
-		this.repaint,
-		this.resolution,
-	}) : super(repaint: repaint);
+  ShaderPainter({
+    this.ssirShader,
+    this.shader,
+    this.repaint,
+    this.resolution,
+  }) : super(repaint: repaint);
 
-	@override
-	void paint(Canvas canvas, Size size) {
-		resolution.value
-			..x = size.width
-			..y = size.height;
+  @override
+  void paint(Canvas canvas, Size size) {
+    resolution.value
+      ..x = size.width
+      ..y = size.height;
 
-		ssirShader.writeUniformData(shader.setFloatUniform);
-		shader.refresh();
-		canvas.drawRect(
-			Offset.zero & size,
-			Paint()..shader = shader,
-		);
-	}
+    ssirShader.writeUniformData(shader.setFloatUniform);
+    shader.refresh();
+    canvas.drawRect(
+      Offset.zero & size,
+      Paint()..shader = shader,
+    );
+  }
 
-	@override
-	bool shouldRepaint(ShaderPainter old) => true;
+  @override
+  bool shouldRepaint(ShaderPainter old) => true;
 }
